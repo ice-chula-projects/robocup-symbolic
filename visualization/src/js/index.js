741492bb013fabb45489718f377411e484b84a90
lolerwastaken <waritsuntrarom@gmail.com>
2026-04-11 23:56:14 +0700

    Remove function in js

--- visualization/src/js/index.js
+++ visualization/src/js/index.js
@@ -4,19 +4,9 @@ const playback = new Playback(document.getElementById("gameLogFileInput"), docum
 const camera = new Camera(document.getElementById("canvas"), playback);
 const fileInput = document.getElementById("gameLogFileInput");
 const fileName = document.getElementById("fileName");
-
 playback.start();
 camera.start();
 //@ts-ignore
 window.playback = playback;
 //@ts-ignore
 window.camera = camera;
-
-// Display file name in UI
-fileInput.addEventListener("change", () => {
-    if (fileInput.files.length > 0) {
-        fileName.textContent = fileInput.files[0].name;
-    } else {
-        fileName.textContent = "No file selected";
-    }
-});
