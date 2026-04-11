79caee27c05ec51edac3d22bd5cfa439e052ecd4
lolerwastaken <waritsuntrarom@gmail.com>
2026-04-11 23:44:51 +0700

    Added cool UI, favicon, added function in index.js

--- visualization/src/js/index.js
+++ visualization/src/js/index.js
@@ -2,9 +2,21 @@ import Camera from "./Camera.js";
 import Playback from "./Playback.js";
 const playback = new Playback(document.getElementById("gameLogFileInput"), document.getElementById("loadButton"));
 const camera = new Camera(document.getElementById("canvas"), playback);
+const fileInput = document.getElementById("gameLogFileInput");
+const fileName = document.getElementById("fileName");
+
 playback.start();
 camera.start();
 //@ts-ignore
 window.playback = playback;
 //@ts-ignore
 window.camera = camera;
+
+// Display file name in UI
+fileInput.addEventListener("change", () => {
+    if (fileInput.files.length > 0) {
+        fileName.textContent = fileInput.files[0].name;
+    } else {
+        fileName.textContent = "No file selected";
+    }
+});
